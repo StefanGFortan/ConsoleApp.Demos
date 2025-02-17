126fe99086431b1f2e2c7a25e2d584cd5e0e087d
StefanGFortan <stefan.gfortan@gmail.com>
2025-02-17 09:48:01 +0200

    Added Exception Handling and Modified Calculator

---
+++ ConsoleApp.ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp.ExceptionHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"You are {age} years old");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Your age value was incorect, please try numbers");
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("thank you for using this program");
+            }
+            
+        }
+    }
+}
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -13,61 +13,82 @@ namespace ConsoleApp.SampleCalculator
             Console.WriteLine(" /  |  |     |");
             Console.WriteLine("/___|  |_____|\n");
 
+            //Variable declaration
             int choise = 0;
+
             //Show calcultaor options
             while (choise != -1)
             {
-                Console.Write("Please select an operation (-1 to exit) : " +
-                    "\n 1.Adition " +
-                    "\n 2.Substraction " +
-                    "\n 3. Multiplication " +
-                    "\n 4.Division " +
-                    "\n 5. Fibonacci sequence" +
-                    "\n Enter please the number for the operation option:");
-                choise = Convert.ToInt32(Console.ReadLine());
-
-                if(choise == -1)
+                try
                 {
-                    break;
+                    //declare variables
+                    int num1 = 0, num2 = 0, answer = 0;
+                    Console.Write("Please select an operation (-1 to exit) : " +
+                        "\n 1.Adition " +
+                        "\n 2.Substraction " +
+                        "\n 3. Multiplication " +
+                        "\n 4.Division " +
+                        "\n 5. Fibonacci sequence" +
+                        "\n Enter please the number for the operation option:");
+                    choise = Convert.ToInt32(Console.ReadLine());
+
+                    if (choise == -1)
+                    {
+                        break;
+                    }
+
+                    //Promt for user input
+                    Console.Write("Please enter the first number:");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Please enter the second number:");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+
+                    //Decide which operation is needed based on selected option
+                    switch (choise)
+                    {
+                        case 1:
+                            answer = num1 + num2;
+                            break;
+                        case 2:
+                            answer = num1 - num2;
+                            break;
+                        case 3:
+                            answer = num1 * num2;
+                            break;
+                        case 4:
+                            answer = num1 / num2;
+                            break;
+                        case 5:
+                            for (int i = num1; i <= num2; i++)
+                            {
+                                answer += i;
+                            }
+                            break;
+                        default:
+                            throw new Exception("Invalid menu item selected.");
+                    }
+
+                    //print output
+                    Console.WriteLine(answer);
                 }
-                //Promt for user input
-                Console.Write("Please enter the first number:");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Please enter the second number:");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-
-                //Decide which operation is needed based on selected option
-                int answer = 0;
-                switch (choise)
+                catch (DivideByZeroException)
                 {
-                    case 1:
-                        answer = num1 + num2;
-                        break;
-                    case 2:
-                        answer = num1 - num2;
-                        break;
-                    case 3:
-                        answer = num1 * num2;
-                        break;
-                    case 4:
-                        answer = num1 / num2;
-                        break;
-                    case 5:
-                        for (int i = num1; i <= num2; i++)
-                        {
-                            answer += i;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Invalid option");
-                        break;
+                    Console.WriteLine("Cannot devide by zero");
+
                 }
-                //print output
-                Console.WriteLine(answer);
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
             }
-
-            
-        }
+            Console.WriteLine("Thank you for using this calculator");
+       }
     }
 }
